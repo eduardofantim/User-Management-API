1d9e806bdcf9c2516b76a5a1949d2b1f90a98aa4
Eduardo Fantim <edugbf5@gmail.com>
2026-05-13 16:53:26 -0300

    build: Configure controller mapping in the pipeline.

--- UserManagement.Api/Program.cs
+++ UserManagement.Api/Program.cs
@@ -8,6 +8,8 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddOpenApi();
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+builder.Services.AddControllers();
+
 
 var app = builder.Build();
 
@@ -18,7 +20,7 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
+app.MapControllers();
 
 
 app.Run();
